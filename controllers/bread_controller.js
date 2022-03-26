22b163b6b2ade208022c1af6fc0a2fe4cc4120b9
Paul  Vivar <Apvivar92@gmail.com>
2022-03-26 10:30:44 -0700

    sending index and show data

--- controllers/bread_controller.js
+++ controllers/bread_controller.js
@@ -1,15 +1,31 @@
-const express = require('express')
-const breads = express.Router()
-const Bread = require('../models/bread.js')
+//DEPENDENCIES
+const express = require('express');
+
+
+//CONFIGURATION
+const breads = express();
+const Bread = require('../models/bread.js');
 
 // INDEX
-breads.get('/', (req,res) => {
-  res.send('This is the index at /breads')
+breads.get('/', (req, res) => {
+  res.render('Index',
+    {
+      breads: Bread
+    }
+  )
+// res.send(Bread)
 })
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-  res.send(Bread[req.params.arrayIndex])
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.send('404')
+  }
 })
 
-module.exports = breads
+//EXPORTS
+module.exports = breads;
--- server.js
+++ server.js
@@ -7,16 +7,25 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+app.use(express.static('public'))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads')
 });
 
 // BREADS
-const breadsController = require(
-  './controllers/bread_controller.js'
-  )
+const breadsController = require('./controllers/bread_controller.js')
   app.use('/breads', breadsController)
+
+//Wild card 404 page
+app.get('*', (req,res) => {
+  res.send('404 error: page does not exist');
+});
 
 // LISTEN
 app.listen(PORT, () => {
